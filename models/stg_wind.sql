99a5da1e4b11e5fa320e4dc6ab961e30740eabcd
SurpraiS <surprais23@gmail.com>
2026-03-22 18:34:45 +0200

    Refactor wind generation SQL query for aggregation
    Updated SQL query to include GROUP BY clause and type casting.

--- models/stg_wind.sql
+++ models/stg_wind.sql
@@ -1,8 +1,9 @@
 {{ config(materialized='table') }}
 
 SELECT
-    -- Minutes in Hours
-    DATE_TRUNC('hour', start_time AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Helsinki') AS start_time_local,
-    ROUND(AVG(value), 2) AS avg_wind_mw
+
+    DATE_TRUNC('hour', start_time::timestamp AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Helsinki') AS start_time_local,
+
+    ROUND(AVG(value::numeric), 2) AS avg_wind_mw
 FROM {{ source('bronze', 'raw_wind_generation') }}
 GROUP BY 1
