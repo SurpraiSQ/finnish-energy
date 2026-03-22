9390191d1a5cdf88c1ac51868582342365ce350f
SurpraiS <surprais23@gmail.com>
2026-03-22 17:36:29 +0200

    Update stg_prices.sql for incremental materialization
    Changed materialization strategy to incremental and added data filtering for incremental loads.

--- models/stg_prices.sql
+++ models/stg_prices.sql
@@ -1,7 +1,16 @@
-{{ config(materialized='table') }}
+{{
+  config(
+    materialized='incremental',
+    unique_key='start_time_local'
+  )
+}}
 
 SELECT
     price AS price_c_kwh,
-    -- UTC in UTC+2
     start_date AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Helsinki' AS start_time_local
 FROM {{ source('bronze', 'raw_spot_prices') }}
+
+{% if is_incremental() %}
+  -- Data filter
+  WHERE start_date AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Helsinki' > (select max(start_time_local) from {{ this }})
+{% endif %}
