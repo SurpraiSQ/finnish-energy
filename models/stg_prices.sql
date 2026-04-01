bf767120d984d97c7e80551ba6a858d009e4a07a
SurpraiS <surprais23@gmail.com>
2026-04-01 11:59:21 +0300

    Refactor stg_prices.sql to use CTE for deduplication
    Refactor SQL to include CTE for deduplication and improve readability.

--- models/stg_prices.sql
+++ models/stg_prices.sql
@@ -5,12 +5,30 @@
   )
 }}
 
+WITH raw_data AS (
+    SELECT
+        price AS price_c_kwh,
+        start_date AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Helsinki' AS start_time_local
+    FROM {{ source('bronze', 'raw_spot_prices') }}
+),
+
+deduplicated_data AS (
+    SELECT
+        price_c_kwh,
+        start_time_local,
+        -- row numbers
+        ROW_NUMBER() OVER(PARTITION BY start_time_local ORDER BY start_time_local) as rn
+    FROM raw_data
+)
+
+-- Unique rows
 SELECT
-    price AS price_c_kwh,
-    start_date AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Helsinki' AS start_time_local
-FROM {{ source('bronze', 'raw_spot_prices') }}
+    price_c_kwh,
+    start_time_local
+FROM deduplicated_data
+WHERE rn = 1
 
 {% if is_incremental() %}
-  -- Data filter
-  WHERE start_date AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Helsinki' > (select max(start_time_local) from {{ this }})
+  -- Filter
+  AND start_time_local > (SELECT MAX(start_time_local) FROM {{ this }})
 {% endif %}
