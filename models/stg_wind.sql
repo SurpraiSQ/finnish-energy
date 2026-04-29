d7ed2eb0147d8623f240393db7bfaac2e3026a78
SurpraiS <surprais23@gmail.com>
2026-04-29 17:14:38 +0300

    Delete models/stg_wind.sql

--- models/stg_wind.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT
-
-    DATE_TRUNC('hour', start_time::timestamp AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Helsinki') AS start_time_local,
-
-    ROUND(AVG(value::numeric), 2) AS avg_wind_mw
-FROM {{ source('bronze', 'raw_wind_generation') }}
-GROUP BY 1
