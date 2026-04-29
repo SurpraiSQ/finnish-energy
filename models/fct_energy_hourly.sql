0f9da8479f950fa65cb6ddd6fe39ced5b5469d03
SurpraiS <surprais23@gmail.com>
2026-04-29 16:53:27 +0300

    Integrate generation data into fct_energy_hourly.sql
    Added generation data from stg_generation_hourly to the query.

--- models/fct_energy_hourly.sql
+++ models/fct_energy_hourly.sql
@@ -1,20 +1,34 @@
 {{ config(materialized='table') }}
 
 WITH prices AS (
-    --  ref() tells dbt: "to take stg_prices"
     SELECT * FROM {{ ref('stg_prices') }}
 ),
 
-wind AS (
-    SELECT * FROM {{ ref('stg_wind') }}
+generation AS (
+    -- New layer
+    SELECT * FROM {{ ref('stg_generation_hourly') }}
 )
 
 SELECT
-    p.start_time_local,
+    -- COALESCE first non-0. 
+    -- if there is no price, from time and same backwards.
+    COALESCE(p.start_time_local, g.hour) AS start_time_local,
+    
+    -- Price Data
     p.price_c_kwh,
-    w.avg_wind_mw,
-    CASE WHEN p.price_c_kwh < 0 THEN true ELSE false END AS is_negative_price
+    CASE WHEN p.price_c_kwh < 0 THEN true ELSE false END AS is_negative_price,
+    
+    -- Generation Data
+    g.wind_mw,
+    g.nuclear_mw,
+    g.hydro_mw,
+    g.other_generation_mw,
+    g.total_production_mw
+
 FROM prices p
-LEFT JOIN wind w
-    ON p.start_time_local = w.start_time_local
-ORDER BY p.start_time_local DESC
+-- FULL OUTER JOIN
+FULL OUTER JOIN generation g
+    ON p.start_time_local = g.hour
+
+-- Sort by time
+ORDER BY start_time_local DESC
